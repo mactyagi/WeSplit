27b9696236b0b6b9ca288e7316a41a1b5fa71e5a
manukant tyagi <manukant.tyagi@senpiper.com>
2023-12-01 10:26:23 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Admin on 01/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
